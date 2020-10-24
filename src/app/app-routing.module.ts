2e0855d378009ae25e41ee71b3bb9d76380daaa0
SezerK <sezerkartal@Sezer-MacBook-Pro.local>
2020-10-24 22:55:51 +0300

    baseHref reverted.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'products',
-    loadChildren: () => import('./modules/eagle-products/eagle-products.module').then(m => m.EagleProductsModule)
-  }
+  { path: 'products', loadChildren: () => import('./modules/eagle-products/eagle-products.module').then(m => m.EagleProductsModule) },
+  { path: '', loadChildren: () => import('./app.module').then(m => m.AppModule) }
 ];
 
 @NgModule({
